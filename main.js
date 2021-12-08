4191aade111c3978a428745570a08c74d11b88c3
Preetam Varun <preetamarun99@gmail.com>
2021-12-08 20:25:19 +0530

    start writing js and remove score card from the UI

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+let player1Turn = true, player2Turn = false;
 
+const player1finalScore = document.getElementById('p1Fscore');
+const player1currentScore = document.getElementById('p1Cscore');
+const player2finalScore = document.getElementById('p2Fscore');
+const player2currentScore = document.getElementById('p2Cscore');
